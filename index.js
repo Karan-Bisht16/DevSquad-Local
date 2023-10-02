036d98dc607ebc2167a77b6624506f7f573e92d1
Karan Bisht <karan161003@gmail.com>
2023-10-02 17:57:47 +0530

    modified:   index.js 	renamed:    public/css/style_index.css -> public/css/style_home.css 	renamed:    public/js/script_index.js -> public/js/script_home.js 	renamed:    views/about_us.html -> views/about_us.ejs 	new file:   views/donate.ejs 	deleted:    views/donate.html 	new file:   views/feedback.ejs 	deleted:    views/feedback.html 	new file:   views/home.ejs 	deleted:    views/index.html 	new file:   views/partials/footer.ejs 	new file:   views/partials/header.ejs 	new file:   views/sign_up.ejs 	deleted:    views/sign_up.html

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ app.set('views', __dirname+'/views');
 app.get('/', (req, res)=>{
     res.render("home.ejs")
 });
+app.get('/donate', (req, res)=>{
+    res.render("donate.ejs")
+});
+app.get('/about_us', (req, res)=>{
+    res.render("about_us.ejs")
+});
+app.get('/feedback', (req, res)=>{
+    res.render("feedback.ejs")
+});
+app.get('/sign_up', (req, res)=>{
+    res.render("sign_up.ejs")
+});
 
 app.listen(port, ()=>{
     console.log(`Server is starting on port ${port}.`);
